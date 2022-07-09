be9a4fb899d27d492dc7b1c0548e605744f0268d
kayak mmx <kayak_mmx@hotmail.com>
2022-07-09 13:03:02 -0500

    unicafe step 3

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -4,6 +4,17 @@ const Button = (props) => (
   <button onClick={props.handleClick}>{props.text}</button>
 )
 
+const Statistics = (props) => (
+  <div>
+    <h1>Statistics</h1>
+    <p>Good {props.good}</p>
+    <p>Neutral {props.neutral}</p>
+    <p>Bad {props.bad}</p>
+    <p>All {props.totalFeedback}</p>
+    <p>Average {props.average}</p>
+    <p>Positive {props.positive} %</p>
+  </div>
+)
 
 const App = () => {
   //save clicks of each button to its own state
@@ -37,13 +48,7 @@ const App = () => {
       <Button handleClick={() => setFeedback("good", good + 1)} text="Good" />
       <Button handleClick={() => setFeedback("neutral", neutral + 1)} text="Neutral" />
       <Button handleClick={() => setFeedback("bad", bad + 1)} text="Bad" />
-      <h1>Statistics</h1>
-      <p>Good {good}</p>
-      <p>Neutral {neutral}</p>
-      <p>Bad {bad}</p>
-      <p>All {totalFeedback}</p>
-      <p>Average {average}</p>
-      <p>Positive {positive}</p>
+      <Statistics good={good} neutral={neutral} bad={bad} totalFeedback={totalFeedback} average={average} positive={positive} />
     </div>
   );
 }
